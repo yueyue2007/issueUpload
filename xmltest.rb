406cca5fb14915a01e3455d7b481893ce963afd9
dh zhong <zhong_dh@126.com>
2014-01-10 00:03:45 +0800

    first create

---
+++ xmltest.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+require 'rexml/document'
+require 'base64'
+include REXML
+
+file = File.open("issue-45.xml")
+
+doc = Document.new file
+root = doc.root 
+
+articles = root.elements.to_a("//section/article")
+articles.each do |article|
+	#put article title
+	p article.elements[1].text 
+	galley = article.elements[5]
+
+	#tiqu pdf base64 and save 
+	filename = galley.elements["file"].elements["embed"].attributes["filename"]
+	pdfContent = Base64.decode64(galley.elements["file"].elements["embed"].text )
+	File.open(filename,'w+') do |f|
+		f << pdfContent 
+	end
+	
+	# 将pdf转为html
+	pdf2html = "pdf2htmlEX  "
+	pdf2html << filename
+	# pdf2html << "  temp.html"
+	# system(pdf2html)
+	# 将html加入到xml中
+
+
+	# 保存到新文件中
+
+	new_file = File.new("new.xml","w+")
+	doc.write new_file 
+	# 将所有的pdf文件和成一个，并专程html格式
+
+
+end
